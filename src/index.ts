dd4d086f6247b548573a0736baa13d10012008bb
vladnen97 <darkdescent1991@gmail.com>
2023-11-01 20:30:39 +0300

    feat: add handle response

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,33 @@ const app: Application = express()
 const port = 3000
 
 
-const products = [{title: 'tomato'}, {title: 'orange'}]
+const products = [{id: 1, title: 'tomato'}, {id: 2, title: 'orange'}]
 const addresses = [{id: 1, value: 'Nezalejnosti 12'}, {id: 2, value: 'Selisckaya 11'}]
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
 app.get('/products', (req: Request, res: Response) => {
-    res.send(products)
+     if (req.query.title) {
+         const title = req.query.title.toString()
+         res.send(products.filter(el => el.title.indexOf(title) !== -1))
+     } else {
+         res.send(products)
+     }
 })
-
-app.get('/products/:title', (req: Request, res: Response) => {
-    const product = products.find(p => p.title === req.params.title)
+app.get('/products/:id', (req: Request, res: Response) => {
+    const product = products.find(p => p.id === +req.params.id)
 
     if (product) {
         res.send(product)
     } else {
-        res.status(404).send('Title not found :(')
+        res.status(404).send('Product not found :(')
     }
 })
 
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
 })
-
 app.get('/addresses/:id', (req: Request, res: Response) => {
     const address = addresses.find(el => el.id === +req.params.id)
     if (address) {
@@ -36,6 +39,9 @@ app.get('/addresses/:id', (req: Request, res: Response) => {
         res.status(404).send('Sorry address does not exist :(')
     }
 })
+
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
