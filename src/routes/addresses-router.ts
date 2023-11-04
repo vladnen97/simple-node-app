7daa2b5ed1224c282bb11f35b03b4f363ed9f828
vladnen97 <darkdescent1991@gmail.com>
2023-11-04 17:33:33 +0300

    feat: addresses routes

---
+++ src/routes/addresses-router.ts
@@ -0,0 +1,19 @@
+import {Request, Response, Router} from 'express';
+import {addressesRepository} from '../repositories/addresses-repository';
+
+
+
+export const addressesRoutes = Router({})
+
+addressesRoutes.get('/', (req: Request, res: Response) => {
+    res.send(addressesRepository.getAddresses())
+})
+addressesRoutes.get('/:id', (req: Request, res: Response) => {
+    const address = addressesRepository.getAddressesById(+req.params.id)
+
+    if (address) {
+        res.send(address)
+    } else {
+        res.status(404).send('Sorry address does not exist :(')
+    }
+})
