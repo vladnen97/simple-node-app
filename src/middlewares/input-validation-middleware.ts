b0df38d90326e750606754e51fc3694aea61b78d
vladnen97 <darkdescent1991@gmail.com>
2023-11-04 20:00:44 +0300

    feat: custom middkeware for errors

---
+++ src/middlewares/input-validation-middleware.ts
@@ -0,0 +1,10 @@
+import {NextFunction, Request, Response} from 'express';
+import {validationResult} from 'express-validator';
+
+export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).send({errors: errors.array()})
+    }
+    next()
+}
