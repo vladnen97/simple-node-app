e31914f0731e5c4f53ec6533e728d56c6bb76948
vladnen97 <darkdescent1991@gmail.com>
2023-11-06 00:54:02 +0300

    fix: add db const and products collection

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,20 +1,26 @@
 import {MongoClient} from 'mongodb'
+import {ProductModel} from './types';
 
 // Replace the placeholder with your Atlas connection string
 const uri = process.env.mongoURI || 'mongodb://127.0.0.1:27017'
 
 export const client = new MongoClient(uri)
 
+const db = client.db('shop')
+
+export const productsCollection = db.collection<ProductModel>('products')
+
+
 export async function run() {
     try {
         // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
+        await client.connect()
 
         // Send a ping to confirm a successful connection
-        await client.db('storage').command({ ping: 1 });
-        console.log("You successfully connected to MongoDB!");
-    } finally {
+        await client.db('storage').command({ping: 1})
+        console.log('You successfully connected to MongoDB!');
+    } catch {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        await client.close()
     }
 }
